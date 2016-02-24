9fca6d48a26c4ffd99cd5b503ec2161706fed2d1
jinjiacun123 <jinjiacuncucu@sina.com>
2016-02-24 16:57:44 +0800

    new

--- src/com/jim/magazine/MainActivity.java
+++ src/com/jim/magazine/MainActivity.java
@@ -1,172 +1,155 @@
 package com.jim.magazine;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-import com.jim.magazine.R;
-
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTabHost;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.ImageView;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.os.Handler;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 
-/**
- * 主页的activity
- * 主要是一个fragmentTabhost
- * 
- * @author jim 
- */
-@SuppressLint("NewApi")
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends SlidingFragmentActivity {
 
-	private FragmentTabHost ftabhost;// 定义一个fragmenttabhost
-	private String[] tabtag = { "home_page",
-			                    "bookself", 
-			                    "group", 
-			                    "mine", };// 给tab取得别名
-	private String[] tabtext = { "首页",
-			                     "书架" ,
-			                     "圈子", 
-			                     "我绘" };// 选项卡里面设置的名字
-	private static Class[] cla = { HomeFragment.class,
-		                           BookselfFragment.class,
-		                           GroupFragment.class,
-		                           MineFragment.class };
-	// 没有被选中的图片的id
-	private int[] imageid = { R.drawable.homepage_a,
-			                  R.drawable.homepage_a, 
-			                  R.drawable.exosure_a,
-			                  R.drawable.mine_a };
-	private LayoutInflater inflater;// 布局解析器
-	private SharedPreferences preferences;
-	private SharedPreferences.Editor editor;
-	private String main_type = "";
+	protected static final String TAG = "MainActivity";
+	private Context mContext;
+	private ImageButton mHome,mConstact,mDeynaimic,mSetting;
+	private View mPopView;
+	private View currentButton;
 	
-	//推送
-	private String mDeviceID;
-	private long user_id;
-	private String token;
-
+	private TextView app_cancle;
+	private TextView app_exit;
+	private TextView app_change;
+	
+	private int mLevel=1;
+	private PopupWindow mPopupWindow;
+	private LinearLayout buttomBarGroup;
+	
+	ListView listView ;
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//透明状态栏
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        //透明导航栏
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
- 
-		initDate();
-	}
+		mContext=this;
 		
-	Handler handler = new Handler(){
-		@Override
-		public void handleMessage(android.os.Message msg){
-			switch(msg.what){
-			case 0:
-				String result = msg.obj.toString();
-				/*
-				final PushInfo pushInfo = JsonUtil.getPushInfo(result);
-				if(pushInfo != null){
-					switch(pushInfo.getIs_sucess()){
-					case 0:
-						Log.i("push", "success" + pushInfo.getIs_sucess());
-						break;
-					case -1:
-						Log.i("push", "failure" + pushInfo.getIs_sucess());
-						break;
-					}
-				}
-				*/
+		findView();
+		init();
+		
+		setBehindContentView(R.layout.leftmenu);
+		// configure the SlidingMenu
+		SlidingMenu menu = getSlidingMenu();
+		menu.setMode(SlidingMenu.LEFT);
+		menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		menu.setShadowWidthRes(R.dimen.shadow_width);
+		menu.setShadowDrawable(R.drawable.shadow);
+		
+		menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		menu.setFadeDegree(0.35f);
+		
+        listView = (ListView) findViewById(R.id.navigate_list);
+        
+        String[] values = new String[] { "Android List View", 
+                                         "Adapter implementation",                                         
+                                        };
+       
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+          android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        listView.setAdapter(adapter); 
+	}
+	
+	private void findView(){
+		mPopView=LayoutInflater.from(mContext).inflate(R.layout.app_exit, null);
+		buttomBarGroup=(LinearLayout) findViewById(R.id.buttom_bar_group);
+		mHome=(ImageButton) findViewById(R.id.buttom_news);
+		
+		app_cancle=(TextView) mPopView.findViewById(R.id.app_cancle);
+		app_change=(TextView) mPopView.findViewById(R.id.app_change_user);
+		app_exit=(TextView) mPopView.findViewById(R.id.app_exit);
+	}
+	
+	private void init(){
+		mHome.setOnClickListener(newsOnClickListener);
+		
+		mHome.performClick();
+		
+		mPopupWindow=new PopupWindow(mPopView, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, true);
+		
+		app_cancle.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mPopupWindow.dismiss();
 			}
+		});
+		
+		app_change.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent=new Intent(mContext, LoginActivity.class);
+				startActivity(intent);
+				((Activity)mContext).overridePendingTransition(R.anim.activity_up, R.anim.fade_out);
+				finish();
+			}
+		});
+		
+		app_exit.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+	}
+	
+	private OnClickListener newsOnClickListener=new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			FragmentManager fm=getSupportFragmentManager();
+			FragmentTransaction ft=fm.beginTransaction();
+			HomeFragment newsFatherFragment= new HomeFragment();
+			ft.replace(R.id.fl_content, newsFatherFragment,MainActivity.TAG);
+			ft.commit();
+			setButton(v);
+			
 		}
 	};
 	
-
-	private void initDate() {
-		inflater = LayoutInflater.from(MainActivity.this);
-		ftabhost = (FragmentTabHost) this.findViewById(android.R.id.tabhost);
-		FragmentManager t = getSupportFragmentManager();
-		//ftabhost.setup(this, getSupportFragmentManager(), com.jim.magazine.R.id.realtabcontent);
-		int my_id = R.id.realtabcontent;
-//		ftabhost.setup(this, getSupportFragmentManager());
-		ftabhost.setup(this, getSupportFragmentManager(), my_id);
-		//ftabhost.setup(getActivity(), getChildFragmentManager(), my_id);
-		preferences = this.getSharedPreferences("Statuse",
-				Context.MODE_PRIVATE);
-		editor = preferences.edit();
-		
-		
-		for (int i = 0; i < tabtext.length; i++) {
-			ftabhost.addTab(
-					ftabhost.newTabSpec(this.tabtag[i]).setIndicator(getView(i)),
-					MainActivity.cla[i], null);
+	private void setButton(View v){
+		if(currentButton!=null&&currentButton.getId()!=v.getId()){
+			currentButton.setEnabled(true);
 		}
+		v.setEnabled(false);
+		currentButton=v;
 	}
 
-	private View getView(int i) {
-		View view = inflater.inflate(R.layout.activity_main_tabview, null);
-		TextView tabtext = (TextView) view.findViewById(R.id.tvtext);
-		ImageView tabicon = (ImageView) view.findViewById(R.id.tabicon);
-		tabtext.setText(this.tabtext[i]);
-		switch (i) {
-		case 0:
-			tabicon.setBackgroundResource(R.drawable.selector_tab_homepage);
-			break;
-		case 1:
-			tabicon.setBackgroundResource(R.drawable.selector_tab_rankinglist);
-			break;
-		case 2:
-			tabicon.setBackgroundResource(R.drawable.selector_tab_exosure);
-			break;
-		case 3:
-			tabicon.setBackgroundResource(R.drawable.selector_tab_mine);
-			break;
-		}
-		return view;
-	}
-
-	
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_BACK) {
-			exitBy2Click(); // 调用双击退出函数
+		if(keyCode==KeyEvent.KEYCODE_MENU){
+			mPopupWindow.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#b0000000")));
+			mPopupWindow.showAtLocation(buttomBarGroup, Gravity.BOTTOM, 0, 0);
+			mPopupWindow.setAnimationStyle(R.style.app_pop);
+			mPopupWindow.setOutsideTouchable(true);
+			mPopupWindow.setFocusable(true);
+			mPopupWindow.update();
 		}
-		return false;
+		return super.onKeyDown(keyCode, event);
+		
 	}
-   
-	/**
-	 * 双击退出函数
-	 */
-	
-	private static Boolean isExit = false;
 
-	private void exitBy2Click() {
-		Timer tExit = null;
-		if (isExit == false) {
-			isExit = true; // 准备退出
-			Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
-			tExit = new Timer();
-			tExit.schedule(new TimerTask() {
-				@Override
-				public void run() {
-					isExit = false; // 取消退出
-				}
-			}, 2000); // 如果2秒钟内没有按下返回键，则启动定时器取消掉刚才执行的任务
-
-		} else {
-			finish();
-		}
-	}	
 }
