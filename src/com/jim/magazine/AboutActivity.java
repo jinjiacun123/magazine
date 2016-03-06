22d51832006ebbea7f14e28761b7849c55731cfa
jinjiacun123 <jinjiacuncucu@sina.com>
2016-03-06 18:00:21 +0800

    修改

--- src/com/jim/magazine/AboutActivity.java
+++ src/com/jim/magazine/AboutActivity.java
@@ -1,7 +1,13 @@
 package com.jim.magazine;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class AboutActivity extends Activity {
-	
+	 @Override
+	    protected void onCreate(Bundle arg0)
+	    {
+	        super.onCreate(arg0);
+	        setContentView(R.layout.activity_about);
+	    }
 }
