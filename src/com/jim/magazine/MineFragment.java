800ac55724113b626b915f1780739a95f74cbfa1
jinjiacun123 <jinjiacuncucu@sina.com>
2016-03-06 17:58:55 +0800

    我绘的修改

--- src/com/jim/magazine/MineFragment.java
+++ src/com/jim/magazine/MineFragment.java
@@ -1,11 +1,24 @@
 package com.jim.magazine;
 
+import com.jim.magazine.view.CustomGrid;
+import com.jim.magazine.view.TitleBarView;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MineFragment extends Fragment implements OnClickListener {
 
+	private View mBaseView;
 	public MineFragment() {
 		// TODO Auto-generated constructor stub
 	}
@@ -16,4 +29,39 @@ public class MineFragment extends Fragment implements OnClickListener {
 		
 	}
 
+    @SuppressLint("NewApi")
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState)
+    {
+        //Random r = new Random(System.currentTimeMillis());
+        Bundle b = getArguments();
+        mBaseView=inflater.inflate(R.layout.fragment_mine, null);
+        
+        //登陆
+        ImageView login = (ImageView)mBaseView.findViewById(R.id.head);
+        login.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent itent = new Intent(getActivity(), LoginActivity.class);
+				startActivity(itent);
+			}        	
+        });
+        
+        
+        //如何成为会员
+        ImageView how_to_mem = (ImageView)mBaseView.findViewById(R.id.how_to_mem);
+        how_to_mem.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View arg0)
+        	{
+        		Intent intent = new Intent(getActivity(), HowmemActivity.class);
+        		startActivity(intent);
+        	}
+        });
+    
+		return mBaseView;
+}
 }
