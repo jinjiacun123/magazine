342f931199e18c20ceafca83573bc64c49b196f6
jinjiacun123 <jinjiacuncucu@sina.com>
2016-03-14 17:49:16 +0800

    修改左菜单控制流程

--- src/com/jim/magazine/MainActivity.java
+++ src/com/jim/magazine/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jim.magazine;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu.OnCloseListener;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu.OnOpenListener;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.jim.magazine.fragment.BookselfFragment;
 import com.jim.magazine.fragment.CircleFragment;
@@ -9,6 +11,7 @@ import com.jim.magazine.fragment.HomeFragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -53,7 +56,6 @@ public class MainActivity extends SlidingFragmentActivity {
 		"关于我们",
 	}; 
 	public static  SlidingMenu menu;
-	public static boolean menu_is_show = false;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -76,6 +78,24 @@ public class MainActivity extends SlidingFragmentActivity {
 		menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
 		menu.setFadeDegree(0.35f);
 		menu.showContent();
+		menu.setOnOpenListener(new OnOpenListener(){
+			 @Override
+		        public void onOpen() {
+		            supportInvalidateOptionsMenu();
+		            Log.d("SLIDING DRAWER","OPEN");
+		            ImageButton leftBtn = (ImageButton)findViewById(R.id.title_btn_left);
+		            leftBtn.setImageResource(R.drawable.home_city_red);
+		        }
+		});
+		menu.setOnCloseListener(new OnCloseListener(){
+			@Override
+			public void onClose()
+			{
+				ImageButton leftBtn = (ImageButton)findViewById(R.id.title_btn_left);
+	            leftBtn.setImageResource(R.drawable.home_city_green);
+			}
+		});
+		
 		
         listView = (ListView) findViewById(R.id.navigate_list);
 
@@ -99,7 +119,6 @@ public class MainActivity extends SlidingFragmentActivity {
 			    {
 			    	//mHome.performClick();
 			    	menu.showContent(true);
-			    	menu_is_show = false;
 			    	 Toast.makeText(getApplicationContext(), ((TextView) view).getText(),  Toast.LENGTH_SHORT).show();
 			    }
 			    	break;
@@ -125,9 +144,9 @@ public class MainActivity extends SlidingFragmentActivity {
 		mPopView=LayoutInflater.from(mContext).inflate(R.layout.app_exit, null);
 		buttomBarGroup=(LinearLayout) findViewById(R.id.buttom_bar_group);
 		mHome   = (ImageButton) findViewById(R.id.buttom_home);//主页
-		mBook   = (ImageButton) findViewById(R.id.buttom_book);//书架
-		mCircle = (ImageButton) findViewById(R.id.buttom_cicle);//圈子
-		mMine   = (ImageButton) findViewById(R.id.buttom_mine);//我绘		
+		mBook     =  (ImageButton) findViewById(R.id.buttom_book);//书架
+		mCircle    =  (ImageButton) findViewById(R.id.buttom_cicle);//圈子
+		mMine     =  (ImageButton) findViewById(R.id.buttom_mine);//我绘		
 		
 		app_cancle=(TextView) mPopView.findViewById(R.id.app_cancle);
 		app_change=(TextView) mPopView.findViewById(R.id.app_change_user);
@@ -140,8 +159,8 @@ public class MainActivity extends SlidingFragmentActivity {
 		mCircle.setOnClickListener(circleOnClickListener);
 		mMine.setOnClickListener(mineOnClickListener);
 		
-		//mHome.performClick();
-		mCircle.performClick();
+		mHome.performClick();
+		//mCircle.performClick();
 		
 		mPopupWindow=new PopupWindow(mPopView, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, true);
 		
@@ -178,8 +197,12 @@ public class MainActivity extends SlidingFragmentActivity {
 			FragmentTransaction ft=fm.beginTransaction();
 			HomeFragment newsFatherFragment= new HomeFragment();
 			ft.replace(R.id.fl_content, newsFatherFragment,MainActivity.TAG);
-			ft.commit();
+			ft.commit();			
 			setButton(v);
+			mHome.setImageResource(R.drawable.home_red);
+			mCircle.setImageResource(R.drawable.circle_green);
+			mBook.setImageResource(R.drawable.book_green);
+			mMine.setImageResource(R.drawable.mine_green);
 		}
 	};
 	
@@ -193,6 +216,10 @@ public class MainActivity extends SlidingFragmentActivity {
 			ft.replace(R.id.fl_content, bookselfFragment,MainActivity.TAG);
 			ft.commit();
 			setButton(v);
+			mHome.setImageResource(R.drawable.home_green);
+			mBook.setImageResource(R.drawable.book_red);
+			mCircle.setImageResource(R.drawable.circle_green);
+			mMine.setImageResource(R.drawable.mine_green);
 		}
 	};
 	
@@ -206,6 +233,10 @@ public class MainActivity extends SlidingFragmentActivity {
 			ft.replace(R.id.fl_content, circleFragment,MainActivity.TAG);
 			ft.commit();
 			setButton(v);
+			mHome.setImageResource(R.drawable.home_green);
+			mBook.setImageResource(R.drawable.book_green);
+			mCircle.setImageResource(R.drawable.circle_red);
+			mMine.setImageResource(R.drawable.mine_green);
 		}
 	};
 	
@@ -219,6 +250,10 @@ public class MainActivity extends SlidingFragmentActivity {
 			ft.replace(R.id.fl_content, mineFragment,MainActivity.TAG);
 			ft.commit();
 			setButton(v);
+			mHome.setImageResource(R.drawable.home_green);
+			mBook.setImageResource(R.drawable.book_green);
+			mCircle.setImageResource(R.drawable.circle_green);
+			mMine.setImageResource(R.drawable.mine_red);
 		}
 	};
 	
