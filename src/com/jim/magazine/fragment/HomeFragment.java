e6703d90e21b6f40350eb7accda5fd7c5823500f
jinjiacun123 <jinjiacuncucu@sina.com>
2016-03-15 11:37:04 +0800

    修改

--- src/com/jim/magazine/fragment/HomeFragment.java
+++ src/com/jim/magazine/fragment/HomeFragment.java
@@ -16,7 +16,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
