b1dca5f7e7514a43576ca45e868989ca643cd059
jinjiacun123 <jinjiacuncucu@sina.com>
2016-04-11 16:07:26 +0800

    修改

--- src/com/jim/magazine/SettingActivity.java
+++ src/com/jim/magazine/SettingActivity.java
@@ -1,7 +1,29 @@
 package com.jim.magazine;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class SettingActivity extends Activity{
 	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_setting);
+		
+		//返回按钮
+		ImageView my_back = (ImageView)findViewById(R.id.back);
+		my_back.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v)
+			{
+				SettingActivity.this.finish();
+			}
+		});
+		
+	}
 }
