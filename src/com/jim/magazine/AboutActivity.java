72c39409154435e71d60bf99a4fe79a609ef0835
jinjiacun123 <jinjiacuncucu@sina.com>
2016-04-12 15:15:04 +0800

    修改

--- src/com/jim/magazine/AboutActivity.java
+++ src/com/jim/magazine/AboutActivity.java
@@ -3,6 +3,7 @@ package com.jim.magazine;
 import android.app.Activity;
 import android.os.Bundle;
 
+//关于我们
 public class AboutActivity extends Activity {
 	 @Override
 	    protected void onCreate(Bundle arg0)
