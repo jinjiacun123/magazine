bef07ffe245eaa790698359e40175e37cbe2e03c
jinjiacun123 <jinjiacuncucu@sina.com>
2016-04-13 20:15:08 +0800

    新增

---
+++ src/com/jim/magazine/entity/ImageEntity.java
@@ -0,0 +1,16 @@
+package com.jim.magazine.entity;
+
+import android.graphics.Bitmap;
+
+public class ImageEntity {
+	private Bitmap image;
+
+	public Bitmap getImage() {
+		return image;
+	}
+
+	public void setImage(Bitmap image) {
+		this.image = image;
+	}
+
+}
